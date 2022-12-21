83404f62d321e6cf45c8b101fa1bcf935d82e584
chicaalexgitthub <chica_alex_gitthub@gmx.es>
2022-12-21 22:39:40 +0100

    21/12/2022, 22:39h. Añado dos menus nuevos con sus respectivas opciones a data.py

--- funciones/data.py
+++ funciones/data.py
@@ -5,3 +5,12 @@ menu0 = "1)Add/Remove/Show Players\n" \
          "5)Reports\n" \
          "6)Exit"
 menu0_options = (1, 2, 3, 4, 5, 6)
+menu01 = "1)Set Game Players\n" \
+         "2)Set Card's Deck\n" \
+         "3)Set Max Rounds(Default 5 Rounds)\n" \
+         "4)Go back"
+menu01_options = (1, 2, 3, 4)
+menu04 = "1)Players With More Earnings\n" \
+         "2)Players With More Games Played\n" \
+         "3)Players With Most Minutes Played"
+menu04_options = (1, 2, 3)
